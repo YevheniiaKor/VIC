c6789926208d3349c1c720dc28b13ed5f8c1a61e
Your Name <your.email@example.com>
2025-11-04 11:26:24 +0100

    nett

--- JS/Undervisning.js
+++ JS/Undervisning.js
@@ -5,3 +5,13 @@ console.log(myName);
 
 const myAge='17';
 console.log(myAge);
+
+console.log(`Hei, jeg heter ${myName} og jeg er ${myAge}`);
+
+console.log(24*60*60);
+
+let x=5;
+let y=10;
+console.log(x+y);
+
+console.log(23/5);
