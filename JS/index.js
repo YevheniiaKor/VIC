791e7d43063b1560288f302e9b6a98f6daf68b86
Your Name <your.email@example.com>
2025-11-13 09:29:54 +0100

    te

--- JS/index.js
+++ JS/index.js
@@ -4,3 +4,12 @@ dokument.getElementById("mySubmit").onclick = function(){
   dokument.getElementById("myH1").textContent = `Hello ${username}`
 
 }
+
+//Frukter
+
+const fruits =[{name: "apple", color: "red", calories: 95},
+       {name: "orange", color: "orange", calories: 45},
+       {name: "banana", color: "yellow", calories: 105},
+       {name: "coconut", color: "white", calories: 159},
+       {name: "pineapple", color: "yellow", calories: 37}];
+console.log(fruits [0].calories);
