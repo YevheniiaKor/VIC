45966ca074b506e8e0e31a73d27e611024e90e64
Your Name <your.email@example.com>
2025-11-20 08:21:20 +0100

    nnk

--- Test Java/Testjava.js
+++ Test Java/Testjava.js
@@ -11,14 +11,18 @@
 
 //console.log(randomNum);
 
-const myButton = document.getElementById("myButton");
+//const myButton = document.getElementById("myButton");
 
-const myLabel = document.getElementById("myLabel");
-const min = 1;
-const max = 6;
-let randomNum;
+//const myLabel = document.getElementById("myLabel");
+//const min = 1;
+//const max = 6;
+//let randomNum;
 
-myButton.onclick = function(){
-    randomNum = Math.floor(Math.random() * max) + min;
-    myLabel.textContent = randomNum;
-}
+//myButton.onclick = function(){
+//    randomNum = Math.floor(Math.random() * max) + min;
+//  myLabel.textContent = randomNum;
+//}
+
+
+
+
