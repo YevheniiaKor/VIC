8ed2e2794168c6d65e3df9d5ab62ea9aec7c85cf
Your Name <your.email@example.com>
2025-11-26 15:27:10 +0100

    nettver2

---
+++ NettsideVersjon2/Nettsideversjon2.js
@@ -0,0 +1,21 @@
+const AppleInput = document.getElementById("Apple");
+const submitAnswer = document.getElementById("submitAnswer");
+const AnswerResult = document.getElementById("AnswerResult");
+ 
+submitAnswer.onclick = function() {
+    let Answer = (AppleInput.value);
+ 
+    if (Answer === "Yes" || Answer === "yes" ) {
+        AnswerResult.textContent = `Welcome to Applepie Club!!`;
+    }
+
+    else if  
+    (Answer === "No" || Answer === "no") {
+        AnswerResult.textContent = `Oh. Well, you must like applepies to be part of our Applepie Club`
+
+    }
+
+    else {
+        AnswerResult.textContent = `You didn't answer the question!`;
+    }
+};
