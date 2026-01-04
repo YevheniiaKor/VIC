1fb4c3a4c9d888c0c3f1ff1740cf1dafe82e507c
Your Name <your.email@example.com>
2026-01-04 16:34:47 +0100

    net2

--- NettsideVersjon2/SSP.js
+++ NettsideVersjon2/SSP.js
@@ -1,65 +1,53 @@
-// Hente elementer fra HTML, alle variabler er ikke hentet enda
+const choices = ["rock", "paper", "scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+let playerScore = 0;
+let computerScore = 0;
 
-// Bilder
-let bilde1 = document.getElementById("spiller1_bilde");
-let bilde2 = document.getElementById("spiller2_bilde");
 
-// Knapper
-let stein = document.getElementById("knapp1");
-let saks = document.getElementById("knapp2");
-let papir = document.getElementById("knapp3");
+function playGame(playerChoice){
 
-let spill = document.getElementById("start");
-let restart_knapp = document.getElementById("restart");
+  const computerChoice = choices[Math.floor(Math.random() * 3)];
+  let result = "";
 
-// Spillets tilstand
-let spiller1_score = 0;
-let maskin_score = 0;
-let runder = 0;
+  if(playerChoice === computerChoice){
+    result = "It's a tie!";
+  }
+  else{
+    switch(playerChoice){
+      case "rock":
+        result = (computerChoice === "scissors") ? "You win!" : "You lose!";
+        break;
+      case "paper":
+        result = (computerChoice === "rock") ? "You win!" : "You lose!";
+        break;
+              case "scissors":
+        result = (computerChoice === "paper") ? "You win!" : "You lose!";
+        break;
+    }
+  }
 
-stein.addEventListener("click", function () {
-  bilde1.src = "NettsideVersjon2/bilder/stein.png";
-});
+  playerDisplay.textContent = `Player: ${playerChoice}`;
+  computerDisplay.textContent = `Computer: ${computerChoice}`;
+  resultDisplay.textContent = result;
 
-saks.addEventListener("click", function () {
-  bilde1.src = "NettsideVersjon2/bilder/saks.png";
-});
+  resultDisplay.classList.remove("greenText", "redText");
 
-papir.addEventListener("click", function () {
-  bilde1.src = "NettsideVersjon2/bilder/papirP.png";
-});
+  switch(result){
+    case "You win!": 
+      resultDisplay.classList.add("greenText");
+      playerScore++;
+      playerScoreDisplay.textContent = playerScore;
+      break;
+    case "You lose!": 
+      resultDisplay.classList.add("redText");
+      computerScore++;
+      computerScoreDisplay.textContent = computerScore;
+      break;
 
-// Funkjson som gir tilbake et random tall mellom 0 og lengde
-function random(lengde) {
-  return Math.floor(Math.random() * lengde);
-}
+  }
 
-// Denne funksjonen skal bestemme maskinens valg
-function maskinValg() {
-  //Tips lag liste av valg og bruk random funksjonen for å hente et tilfeldig valg eventuelt implementer logikken her
-}
-
-// Denne funksjonen skal sette bildene basert på valg
-function settbilder(maskin, spiller) {
-  //logikk for å sette bilder basert på valg
-}
-
-// Denne funksjonen skal bestemme vinneren av runden
-function vinner(maskin, spiller) {
-  //logikk for å bestemme vinneren av runden
-}
-
-// Denne funksjonen skal oppdatere brukergrensesnittet, altså hva klientsiden ser
-function oppdaterUI() {
-  //logikk for å oppdatere brukergrensesnittet, som score og antall runder
-}
-
-//Denne funksjonen skal kjøre spillet, altså en runde
-function spillFunksjon() {
-  //logikk for å spille en runde
-}
-
-// Denne funksjonen skal restarte spillet og dens verdier
-function restart() {
-  //logikk for å nullstille spillet og dets variabler
 }
